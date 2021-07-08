9a7c62ce83374145ddf80b7a15feb7f90a294313
qinxiaojun <qinxiaojun730@sina.com>
2021-07-08 11:29:01 +0800

    上机说明修改

--- src/main/java/com/smart/dao/UserMapper.java
+++ src/main/java/com/smart/dao/UserMapper.java
@@ -1,9 +1,7 @@
 package com.smart.dao;
 
-import com.smart.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,6 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Mapper
 public interface UserMapper {
-    @Select("select count(0) from t_user where user_name = #{username} and password = #{password}")
     boolean hasMatchUser(@Param("username") String username, @Param("password")  String password);
 }
